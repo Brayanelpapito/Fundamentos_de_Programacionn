cf22b2e9c3bee28f09eba5a7e07bb64cde0ede94
Brayanelpapito <106199752+Brayanelpapito@users.noreply.github.com>
2023-09-29 14:57:22 -0600

    Mi primer programa
    Mi primer programa

---
+++ Unidad2/Programa4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace p3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int GC;
+            double GF;
+            string nombre;
+
+
+            Console.WriteLine(" * * Bienvenido * * ");
+
+            Console.Write("Escribre tu nombre:");
+            nombre = Console.ReadLine();
+
+            Console.Write("Escribre tus grados centigrados:");
+            GC = Convert.ToInt32(Console.ReadLine());
+
+            GF = GC * 1.8 + 32;
+
+
+
+            Console.WriteLine(nombre);
+            Console.Write("Despues de hacer calculos bien makiabelicos");
+            Console.Write($" sus grados centigrados son  {GF} grados farenheit");
+            Console.WriteLine("--Gracias");
+
+        }
+    }
+}
