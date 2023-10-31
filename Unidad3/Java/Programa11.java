a245f1f813094c09192f5538f5782ad0462d5df2
Brayanelpapito <106199752+Brayanelpapito@users.noreply.github.com>
2023-10-31 00:40:49 -0600

    Add files via upload

---
+++ Unidad3/Java/Programa11.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa11;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa11 {
+
+    public static void main(String[] args) {
+      int c;
+      c=1;
+       while (c<=100){
+             System.out.print(c);
+             System.out.print("\n ");
+             c = c + 1;}
+    
+       
+       while (c>=-100){
+             System.out.print(c);
+             System.out.print("\n ");
+             c = c - 1;}
+}
+}
