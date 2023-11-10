6879e2f4037241bb448a07096e2acaa4dd88079b
Brayanelpapito <106199752+Brayanelpapito@users.noreply.github.com>
2023-11-09 19:41:28 -0600

    Programa3.cs

--- Programa3.cs
+++ Programa3.cs
@@ -11,20 +11,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.Write("Escribe el total de Temperaturas a procesar:");
             tan = Convert.ToInt32(Console.ReadLine());
             double[] cuantos = new double[tan];
+            Console.WriteLine("                  ");
+            Console.WriteLine("Hola");
 
             for (double c = 0; c < cuantos.Length; c++)
+               
             {
-                Console.Write("Escribe tu Temperaturas:");
+                Console.Write("Escribe tu Temperatura;");
                 er = Convert.ToInt32(Console.ReadLine());
 
 
 
-                F = c*1.8 + 32;
-                k = c + 273.15;
-                Console.WriteLine("Hola");
+                F = (er*1.8) + 32;
+                k = ((F - 32) / 1.8 ) + 273.15;
+
                 Console.WriteLine("Tus Grados Centigrados son:" + er);
                 Console.WriteLine("Tus Grados Farenheit son:" + F);
                 Console.WriteLine("Tus Grados Kelvin son:" + k);
+                Console.WriteLine("                  ");
 
             }
             Console.WriteLine("ADIOOOOOOOOOOOOOOOOS");
