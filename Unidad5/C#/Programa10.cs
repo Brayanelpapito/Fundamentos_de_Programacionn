c85be91874b91ecd3cfdc8e8d125b8dd1e407669
Brayanelpapito <106199752+Brayanelpapito@users.noreply.github.com>
2023-12-10 03:56:41 -0600

    Add files via upload

---
+++ Unidad5/C#/Programa10.cs
@@ -0,0 +1,23 @@
+﻿using System.Text; 
+namespace Programa10;
+
+class Password{
+    static void Main(string[] args){
+        int longitud=8;
+        Console.Write("Password: "+random(longitud));
+    }
+    public static String random(int x){ //x es el tamaño    
+       
+        string letras = "ABSLHDLHKAH/$%#&_jkjsaloqn"; //Doble comilla para string y comillas simpre para caracter
+        StringBuilder cadena = new StringBuilder(x); //Construir caracteres como un arreglo 
+        Random alea = new Random(); //Funcion para que genere un numero aleatorio
+       
+        for(int i=0; i<x; i++){
+        
+           int IndiceCaracter = alea.Next(letras.Length); 
+           cadena.Append(letras[IndiceCaracter]); 
+        
+        }
+        return cadena.ToString(); //toString() el resultado lo conviertes en texto
+    }
+}
